d87499075c41a170508c3c97e73996dd4893b81d
Carururu <cdvsalanga@gmail.com>
2021-10-27 17:11:01 +0800

    Layouts & CSS Modules

--- pages/about.js
+++ pages/about.js
@@ -1,14 +1,15 @@
 import Head from 'next/head'
 
-export default function Home() {
+const about = () => {
   return (
-    <div>
+    <>
       <Head>
         <title>WebDev News</title>
         <meta name='keywords' content='web development, programming' />
       </Head>
-
-      <h1>Welcome to Next</h1>
-    </div>
+      <h1>About</h1>
+    </>
   )
 }
+
+export default about
